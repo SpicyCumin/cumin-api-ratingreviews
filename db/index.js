a09dadf0c5ae5430ad9eb0d2ee119791b41d6292
Ian Bringe <imbringeHR@gmail.com>
2022-04-21 06:57:38 -0400

    Added more config options like to select the DB type and whether or not to hydrate the DB.

--- db/index.js
+++ db/index.js
@@ -1,10 +1,22 @@
 
 //sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
-const db = require('../mongo/index')
-// const db = require('../sequelize/index')
+var dbToLoad = process.env.DB
+var db;
+if (dbToLoad === 'MYSQL') {
+  db = require('../sequelize/index')
+}
+else {
+  db = require('../mongo/index')
+  dbToLoad = "MONGO"
+}
+console.log(`CURRENT DB = ${dbToLoad}`)
 
 
-db.checkForHydration()
+const toHydrate = JSON.parse(process.env.HYDRATE) || false
+console.log(`TO HYDRATE DB = ${toHydrate}`)
+if (toHydrate) {
+  db.checkForHydration()
+}
 
 module.exports = db
 
